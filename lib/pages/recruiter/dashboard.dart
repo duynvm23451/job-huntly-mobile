b2e6c01fe6c962625b70f413b0d58fbfa8d36476
duy23451 <duynvm1711@gmail.com>
2024-08-25 23:17:55 +0900

    implement drawer

--- lib/pages/recruiter/dashboard.dart
+++ lib/pages/recruiter/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:job_huntly_mobile/constant/colors_constant.dart';
 import 'package:job_huntly_mobile/constant/routes.dart';
 import 'package:job_huntly_mobile/provider/token_provider.dart';
 import 'package:provider/provider.dart';
@@ -11,6 +13,10 @@ class RecruiterDashBoard extends StatelessWidget {
     final tokenProvider = Provider.of<TokenProvider>(context);
 
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorsConstant.customNeutral,
+        title: const Text("Recruiter"),
+      ),
       drawer: _drawer(),
       body: Center(
         child: Column(
@@ -37,15 +43,65 @@ class _drawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: ColorsConstant.customNeutral,
       child: ListView(
         children: [
-          ListTile(
-            title: const Text("Item 1"),
+          Container(
+            margin: EdgeInsets.only(left: 16),
+            child: Align(
+                alignment: Alignment.bottomLeft,
+                child: SvgPicture.asset("assets/images/logo.svg")),
           ),
-          ListTile(
-            title: const Text("Item 2"),
-          )
+          SizedBox(
+            height: 28,
+          ),
+          navBarItem("assets/images/home.svg", "Dashboard", context),
+          SizedBox(
+            height: 20,
+          ),
+          navBarItem("assets/images/messages.svg", "Tin nhắn", context),
+          SizedBox(
+            height: 20,
+          ),
+          navBarItem("assets/images/company.svg", "Công ty", context),
+          SizedBox(
+            height: 20,
+          ),
+          navBarItem("assets/images/people.svg", "Ứng viên", context),
+          SizedBox(
+            height: 20,
+          ),
+          navBarItem("assets/images/job.svg", "Công ty", context),
         ],
+      ),
+    );
+  }
+
+  GestureDetector navBarItem(
+      String svgAdress, String title, BuildContext context) {
+    ModalRoute.of(context)?.settings.name;
+    return GestureDetector(
+      onTap: () => {print(ModalRoute.of(context)?.settings.name)},
+      child: Container(
+        margin: EdgeInsets.only(left: 16),
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              svgAdress,
+              color: Colors.grey.shade600,
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey.shade600,
+                  fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
       ),
     );
   }
